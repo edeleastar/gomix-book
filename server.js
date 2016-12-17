e3e550367ab8bd4ce8bc71d60f156167f54e8a6a
Eamonn de Leastar <edeleastar@wit.ie>
2016-12-17 08:36:23 +0000

    first commit of template project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('/', routes);
+
+const listener = app.listen(process.env.PORT | 4000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
